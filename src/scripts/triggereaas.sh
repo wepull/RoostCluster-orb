91712408cd4f12a9cfa1e00569a6a46ce2cc40c9
wepull <vipul.choudhary@zb.io>
2022-12-30 15:37:31 +0530

    test

--- src/scripts/triggereaas.sh
+++ src/scripts/triggereaas.sh
@@ -2,7 +2,7 @@
 ROOST_DIR="/var/tmp/Roost"
 LOG_FILE="$ROOST_DIR/cluster.log"
 
-ORB_ROOST_AUTH=$(eval echo "${ROOST_AUTH_TOKEN}")
+ORB_ROOST_AUTH=$(eval echo "${$ROOST_AUTH}")
 
 trigger_eaas() {
   TRIGGER_IDS=$(curl --location --silent --request POST "https://$ENT_SERVER/api/application/triggerEaasFromCircleCI" \
